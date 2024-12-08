7da8beccc4f3652f893656d29695af3ed5bd018a
Sebastian Andersson <sebastian.andersson@lumenradio.com>
2024-12-08 09:15:32 +0100

    Add ALPHANUMS : [u8] const

--- src/lib.rs
+++ src/lib.rs
@@ -7,5 +7,7 @@ mod graph;
 /// module for maps, 2d points and directions
 mod world;
 
+pub const ALPHANUMS: &[u8; 62] = b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
 pub use graph::*;
 pub use world::*;
