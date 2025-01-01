d506a4a3ecdee81aa342260c6cdca7660dc6cd35
Sebastian Andersson <sebastian.andersson@lumenradio.com>
2025-01-01 19:30:36 +0100

    Make PointAndCost public, add documentation

--- src/world.rs
+++ src/world.rs
@@ -29,25 +29,57 @@ pub use dir::*;
 use num::*;
 pub use point::Point;
 
+/// A struct to keep a Point together with a number.
+///
+/// Used to keep track of Points and a cost, useful when storing them in BinaryHeap etc
+/// for search algorithms.
+///
+/// `Ord` and `PartialOrd` are implemented for it. Only the cost field is compared,
+/// and the lowest cost is the greatest.
+///
+/// # Example:
+/// ```
+/// # use advent_of_tools::*;
+/// let point = Point {x: 1, y: 3};
+/// let pc1 = PointAndCost{cost: 3, point: point * 2};
+/// let pc2 = PointAndCost{cost: 7, point: point};
+/// assert!(pc2.cmp(&pc1) == std::cmp::Ordering::Less);
+/// ```
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-struct CostAndPoint<T: LengthType, U: Num>(U, Point<T>);
+pub struct PointAndCost<T: LengthType, U: Num> {
+    pub cost: U,
+    pub point: Point<T>,
+}
 
 impl<T: LengthType + PartialOrd + Eq + PartialEq, U: Num + Ord + PartialOrd + Eq + PartialEq> Ord
-    for CostAndPoint<T, U>
+    for PointAndCost<T, U>
 {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        other.0.cmp(&self.0)
+        other.cost.cmp(&self.cost)
     }
 }
 
 impl<T: LengthType + PartialOrd + Eq + PartialEq, U: Num + Ord + PartialOrd + Eq + PartialEq>
-    PartialOrd for CostAndPoint<T, U>
+    PartialOrd for PointAndCost<T, U>
 {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(other.0.cmp(&self.0))
+        Some(other.cost.cmp(&self.cost))
     }
 }
 
+/// Map stores ASCII text maps.
+///
+/// # Example
+///
+/// ```
+/// # use advent_of_tools::*;
+///
+/// let map = "####\n@.#.\n.#..\n";
+/// let map : Map<i32> = Map::from_string(map);
+/// assert_eq!(map.get_width(), 4);
+/// assert_eq!(map.get_height(), 3);
+/// assert_eq!(map.find(b'@').len(), 1);
+/// ```
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub struct Map<T: LengthType = Length>
 where
@@ -163,10 +195,32 @@ where
     usize: TryFrom<T>,
     <usize as TryFrom<T>>::Error: std::fmt::Debug,
 {
+    /// Get the width of Map.
+    ///
+    /// The width includes the border.
+    ///
+    /// # Example:
+    /// ```
+    /// # use advent_of_tools::*;
+    /// let map : Map<i16> = Map::from_string_with_border("aa");
+    ///
+    /// assert_eq!(map.get_width(), 4);
+    /// ```
     pub fn get_width(&self) -> T {
         self.width
     }
 
+    /// Get the height of Map.
+    ///
+    /// The height includes the border.
+    ///
+    /// # Example:
+    /// ```
+    /// # use advent_of_tools::*;
+    /// let map = Map::<i32>::from_string_with_border("aa");
+    ///
+    /// assert_eq!(map.get_height(), 3);
+    /// ```
     pub fn get_height(&self) -> T {
         self.height
     }
@@ -175,10 +229,30 @@ where
         usize::try_from(pos.x + pos.y * self.width).expect("Positive index")
     }
 
+    /// Get the tile at a valid position.
+    ///
+    /// # Example
+    /// ```
+    /// # use advent_of_tools::*;
+    /// let map = Map::from_string("ab\ncd\n");
+    ///
+    /// assert_eq!(map.get_at_unchecked(Point{x: 0, y: 1}), b'c');
+    /// ```
     pub fn get_at_unchecked(&self, pos: Point<T>) -> u8 {
         self.data[self.get_index_for(pos)]
     }
 
+    /// Get the tile at a position.
+    ///
+    /// Returns None if the position isn't valid.
+    ///
+    /// # Example
+    /// ```
+    /// # use advent_of_tools::*;
+    /// let map = Map::from_string("ab\ncd\n");
+    ///
+    /// assert_eq!(map.get_at(Point{x: 0, y: 2}), None);
+    /// ```
     pub fn get_at(&self, pos: Point<T>) -> Option<u8> {
         if self.is_inside_map(pos) {
             Some(self.data[self.get_index_for(pos)])
@@ -187,11 +261,27 @@ where
         }
     }
 
+    /// Set the tile at a position.
+    ///
+    /// The position is assumed to be valid.
+    /// It can set tiles in the border as well.
+    ///
+    /// # Example
+    /// ```
+    /// # use advent_of_tools::*;
+    /// let mut map = Map::from_string_with_border("ab\ncd\n");
+    ///
+    /// map.set_at(Point{x: 0, y: 2}, b'!');
+    /// assert_eq!(map.get_at(Point{x: 0, y: 2}), Some(b'!'));
+    /// ```
     pub fn set_at(&mut self, pos: Point<T>, val: u8) {
         let index = self.get_index_for(pos);
         self.data[index] = val
     }
 
+    /// Create a new map of given dimensions.
+    ///
+    /// It is filled with b'.' tiles.
     pub fn new(width: T, height: T) -> Self {
         let mut data =
             Vec::with_capacity(usize::try_from(width * height).expect("Positive number"));
@@ -246,6 +336,11 @@ where
         }
     }
 
+    /// Create a Map from a string.
+    ///
+    /// The string is assumed to end each line with a single linefeed.
+    /// Each line becomes a row in the map.
+    /// All lines are assumed to be of equal width.
     pub fn from_string(s: &str) -> Self
     where
         T: TryFrom<usize>,
@@ -270,6 +365,27 @@ where
         }
     }
 
+    /// Create a Map from a string with a border.
+    ///
+    /// The string is assumed to end each line with a single linefeed.
+    /// Each line becomes a row in the map.
+    /// All lines are assumed to be of equal width.
+    ///
+    /// The map gets a border around it with b'+' in the corners,
+    /// b'-' along the top and bottom edges and b'|' along the sides.
+    ///
+    /// # Example
+    /// ```
+    /// # use advent_of_tools::*;
+    /// let mut map = Map::from_string_with_border("abc\ndef");
+    ///
+    /// assert_eq!(map.get_width(), 5);
+    /// assert_eq!(map.get_height(), 4);
+    ///
+    /// assert_eq!(map.get_at_unchecked(Point{x: 0, y: 2}), b'|');
+    /// assert_eq!(map.get_at(Point{x: 0, y: 0}), Some(b'+'));
+    /// assert_eq!(map.get_at(Point{x: 2, y: 0}), Some(b'-'));
+    /// ```
     pub fn from_string_with_border(s: &str) -> Self
     where
         T: TryFrom<usize>,
@@ -309,35 +425,119 @@ where
         }
     }
 
+    /// Print the map to stdout with an overlay provided by f.
+    ///
+    /// For every tile in the map, the f function will be called
+    /// with the position and the tile at that position.
+    ///
+    /// The returned u8 will be the tile that is printed.
+    ///
+    /// # Example:
+    /// ```
+    /// # use advent_of_tools::*;
+    /// let mut map = Map::<i32>::from_string_with_border("abc\ndef");
+    ///
+    /// map.print_with_overlay(|pos, tile| {
+    ///   if pos.y == 1 {
+    ///      b'='
+    ///   } else {
+    ///      tile
+    ///   }
+    /// })
+    /// ```
     pub fn print_with_overlay<F>(&self, mut f: F)
     where
-        F: FnMut(Point<T>, u8) -> Option<u8>,
+        F: FnMut(Point<T>, u8) -> u8,
     {
         for y in range(Zero::zero(), self.height) {
             for x in range(Zero::zero(), self.width) {
                 let pos = Point { x, y };
                 let mut c = self.get_at_unchecked(pos);
-                if let Some(new_c) = f(pos, c) {
-                    c = new_c;
-                }
+                c = f(pos, c);
                 print!("{}", char::from(c));
             }
             println!();
         }
     }
 
+    /// Print the map to stdout.
+    ///
+    /// # Example:
+    /// ```
+    /// # use advent_of_tools::*;
+    /// let mut map = Map::<i32>::from_string_with_border("abc\ndef");
+    ///
+    /// map.print();
+    /// ```
     pub fn print(&self) {
-        self.print_with_overlay(|_, _| None);
+        self.print_with_overlay(|_, tile| tile);
     }
 
+    /// Iterate over all positions in the map.
+    ///
+    /// The iterator returns a tuple of the position's Point and the tile.
+    ///
+    /// # Example:
+    /// ```
+    /// # use advent_of_tools::*;
+    /// let mut map = Map::<i32>::from_string("abc\ndef");
+    ///
+    /// assert_eq!(map.iter().filter(|&(_pos, tile)| tile == b'a').count(), 1);
+    /// ```
     pub fn iter(&self) -> MapIterator<T> {
         MapIterator::new(self)
     }
 
+    /// Iterate over all neigbors to a position in the map.
+    ///
+    /// The iterator returns a tuple of the neighbor's Point,
+    /// the direction to it and the tile.
+    /// All valid of the 8 neighbors are given.
+    ///
+    /// # Example:
+    /// ```
+    /// # use advent_of_tools::*;
+    /// let mut map = Map::<i32>::from_string("abc\ndef");
+    ///
+    /// let point = Point {x: 0, y: 0};
+    /// assert_eq!(map.neighbors(point).count(), 3);
+    /// for (pos, dir, tile) in map.neighbors(point) {
+    ///   print!("{pos:?}, {dir}, {tile}");
+    /// }
+    /// ```
+    /// This outputs:
+    ///
+    /// ```text
+    /// Point { x: 1, y: 0}, East, b
+    /// Point { x: 0, y: 1}, South, d
+    /// Point { x: 1, y: 1}, SouthEast, e
+    /// ```
     pub fn neighbors(&self, pos: Point<T>) -> MapNeighborIterator<T> {
         MapNeighborIterator::new(self, pos)
     }
 
+    /// Update all tiles with a given area.
+    ///
+    /// `from` is the top left corner of the area,
+    /// `to` is the bottom right corner of the area.
+    ///
+    /// `f` is a function that gets called with the map, the position and the tile
+    /// and its returned tile will set the new value.
+    ///
+    /// The map is double buffered while being transformed so old values
+    /// are returned if `f` asks map about other tiles.
+    ///
+    /// The function returns true if any tiles were changed.
+    ///
+    /// # Example of filling an area with transform:
+    /// ```
+    /// # use advent_of_tools::*;
+    /// let mut map = Map::<i32>::new(10, 10);
+    ///
+    /// let retval = map.transform_area(Point{x: 2, y: 2}, Point{x: 7, y: 6}, |_map, _pos, _tile| b'*');
+    /// assert!(retval);
+    /// assert_eq!(map.find(b'*').len(), 5*4);
+    /// ```
     pub fn transform_area<F>(&mut self, from: Point<T>, to: Point<T>, mut f: F) -> bool
     where
         F: FnMut(&Self, Point<T>, u8) -> u8,
@@ -361,6 +561,19 @@ where
         any_change
     }
 
+    /// Update all tiles.
+    ///
+    /// The function works like `transform_area`, except the whole map
+    /// will be affected.
+    ///
+    /// `f` is a function that gets called with the map, the position and the tile
+    /// and its returned tile will set the new value.
+    ///
+    /// The map is double buffered while being transformed so old values
+    /// are returned if `f` asks map about other tiles.
+    ///
+    /// The function returns true if any tiles were changed.
+    ///
     pub fn transform<F>(&mut self, f: F) -> bool
     where
         F: FnMut(&Self, Point<T>, u8) -> u8,
@@ -392,6 +605,9 @@ where
         }
     }
 
+    /// Check if given position is within the Map's valid area.
+    ///
+    /// If the map has a border, its positions are also valid.
     pub fn is_inside_map(&self, pos: Point<T>) -> bool {
         pos.x >= Zero::zero()
             && pos.y >= Zero::zero()
@@ -399,10 +615,23 @@ where
             && pos.y < self.get_height()
     }
 
-    /// moves pos in dir
+    /// moves pos in the given direction
     ///
-    /// It stops when the next point in that direction is outside of the map or causes f to return
-    /// false
+    /// It stops when the next point in that direction is outside of the map or causes `f` to return
+    /// false.
+    ///
+    /// # Example:
+    /// ```
+    /// # use advent_of_tools::*;
+    /// let mut map = Map::<i32>::new(10, 10);
+    ///
+    /// let position = map.walk_until(Point{x: 5, y: 5}, Dir::West, |_pos, tile| tile != b'.');
+    /// assert_eq!(position, Point {x: 0, y: 5});
+    ///
+    /// map.add_border(b'#');
+    /// let position = map.walk_until(Point{x: 5, y: 5}, Dir::West, |_pos, tile| tile != b'.');
+    /// assert_eq!(position, Point {x: 1, y: 5});
+    /// ```
     pub fn walk_until<F>(&self, pos: Point<T>, dir: Dir, mut f: F) -> Point<T>
     where
         F: FnMut(Point<T>, u8) -> bool,
@@ -520,6 +749,7 @@ where
         }
     }
 
+    /// Finds all tiles matching `needle`.
     pub fn find(&self, needle: u8) -> Vec<Point<T>> {
         self.iter()
             .filter_map(|(p, c)| if c == needle { Some(p) } else { None })
@@ -534,8 +764,11 @@ where
         // TODO: Give path instead?
         let mut expanded = std::collections::HashMap::new();
         let mut to_expand = std::collections::BinaryHeap::new();
-        to_expand.push(CostAndPoint(Zero::zero(), from));
-        while let Some(CostAndPoint(cost, pos)) = to_expand.pop() {
+        to_expand.push(PointAndCost {
+            cost: Zero::zero(),
+            point: from,
+        });
+        while let Some(PointAndCost { cost, point: pos }) = to_expand.pop() {
             if to == pos {
                 return cost;
             }
@@ -552,7 +785,10 @@ where
                 .neighbors(pos)
                 .filter_map(|(pos, dir, c)| f(self, pos, dir, c).map(|step| (step + cost, pos)))
             {
-                to_expand.push(CostAndPoint(new_cost, pos));
+                to_expand.push(PointAndCost {
+                    cost: new_cost,
+                    point: pos,
+                });
             }
         }
         Zero::zero()
